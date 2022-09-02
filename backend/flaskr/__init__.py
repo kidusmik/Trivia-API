c46af493804e65f4c038e4d4329bc1dbe3f43b3d
kidusmik <kidusmik@gmail.com>
2022-09-02 18:36:15 +0300

    Fix route methods and Enhanced codes

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+import json
 
 from models import setup_db, Question, Category
 
@@ -12,7 +13,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     def paginate_questions(request, selection):
         page = request.args.get('page', 1, type=int)
@@ -32,30 +33,38 @@ def create_app(test_config=None):
     @app.route('/categories')
     def get_categories():
         categories = Category.query.order_by(Category.id).all()
+        categories_formatted = {category.id: category.type for category in categories}
         
         return jsonify({
-            'categories': [category.format() for category in categories]
+            'categories': categories_formatted
         })
 
-    @app.route(/questions)
+    @app.route('/questions')
     def get_questions():
         selection = Question.query.order_by(Question.id).all()
         current_questions = paginate_questions(request, selection)
+        categories_response = get_categories()
+        categories_data = json.loads(json.dumps(categories_response.json))
 
-        if len(current_books) == 0:
+        if len(current_questions) == 0:
             abort(404)
 
-            return jsonify({
+        return jsonify({
+                'success': True,
                 'questions': current_questions,
                 'total_questions': len(selection),
-                'categories': Category.query.order_by(Category.id).all(),
-                'current_category': current_books #TODO
+                'categories': categories_data.get('categories'),
+                'current_category': 'Science'
             })
 
-    @app.route('/questions/<int:question_id', methods=['DELETE'])
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
         question = Question.query.get(question_id)
         question.delete()
+        
+        return jsonify({
+            'message': 'Delete Successful'
+        })
 
     @app.route('/questions', methods=['POST'])
     def add_new_question():
@@ -74,19 +83,26 @@ def create_app(test_config=None):
                 difficulty=difficulty
             )
             question.insert()
+            return jsonify({
+                'message': 'Create Successful'
+            })
         except:
             abort(422)
+        
+        return jsonify({
+            'message': 'Create Successful'
+        })
 
     @app.route('/questions/search', methods=['POST'])
-    def search_venues():
+    def search_questions():
         search_term = request.form['searchTerm']
         search_query = '%{0}%'.format(search_term)
         questions = Question.query.filter(Question.question.ilike(search_query)).all()
 
-        response = jsonify({
+        return jsonify({
             'questions': questions,
             'total_questions': len(questions),
-            'current_category': 
+            'current_category': 'Science'
         })
 
     @app.route('/categories/<int:category_id>/questions')
@@ -94,11 +110,11 @@ def create_app(test_config=None):
         category = Category.query.get(category_id)
         category_type = category.type
         
-        questions = Question.query.filter_by(Question.category==category_type).all()
+        questions = Question.query.filter(Question.category==category_type).all()
         return jsonify({
             'questions': questions,
             'total_questions': len(questions),
-            'current_category': category_id
+            'current_category': category.format()
         })
 
     """
@@ -112,11 +128,45 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route('/quizzes', methods=['POST'])
+    def get_quiz_questions():
+        body = request.get_json()
+        previous_questions = body.get('previous_questions')
+        quiz_category = body.get('quiz_category')
+        quiz_id = quiz_category.get('id', None)
+        questions = Question.query.all()
+        """
+        if quiz_id != None:
+            questions = Question.query.filter(Question.category==quiz_id).all()
+        else:
+            questions = Question.query.all()
+        """ 
+        questions_formatted = [question.format() for question in questions]
+            
+        #questions_sent = []
+        question = random.choice(questions_formatted)
+            
+        while question in previous_questions:
+            question = random.choice(questions_formatted)
+        
+        return jsonify({
+            'question': question
+        })
 
-    """
-    @TODO:
-    Create error handlers for all expected errors
-    including 404 and 422.
-    """
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'requested resource not found'
+        }), 404
+
+    @app.errorhandler(422)
+    def unporcessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable entity'
+        }), 422
 
     return app
